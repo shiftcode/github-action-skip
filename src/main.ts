c1cf902a54ec039981340bfcffb45970fab264d6
Michael Wittwer <michael.wittwer@shiftcode.ch>
2019-11-08 16:53:02 +0100

    commit with message sample, to let the build fail

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,10 @@ async function run() {
     const skipOnCommitMsg = core.getInput(INPUT)
     console.log(`skip CI on commit message ${skipOnCommitMsg}`)
 
+    core.startGroup('Expand to see the github context')
     console.log(JSON.stringify(git.context))
+    core.endGroup()
+    
     const commitMessage = git.context.payload.head_commit.message
     if (commitMessage.includes(skipOnCommitMsg)) {
       core.setFailed(`stopping here, because the commit message contains the provided input <${skipOnCommitMsg}>`)
