255b066e4c98015033fae4a17dfcfc88b8033239
michaelwittwer <michael.wittwer@shiftcode.ch>
2022-01-04 11:45:53 +0100

    feat(*): next try

--- src/main.ts
+++ src/main.ts
@@ -8,12 +8,15 @@ async function run() {
     core.debug('start working')
     const skipOnCommitMsg = core.getInput(INPUT)
     console.log(`skip CI on commit message ${skipOnCommitMsg}`)
-
-    const commitMessage = git.context.payload.head_commit.message
-    console.log('commit message', commitMessage)
-    if (commitMessage.includes(skipOnCommitMsg)) {
-      core.setFailed(`stopping here, because the commit message contains the provided input <${skipOnCommitMsg}>`)
-    }
+    core.setFailed(`stopping here, because the commit message contains the provided input <${skipOnCommitMsg}>`)
+// const ghToken = core.getInput()
+//     git.getOctokit()
+//     console.log(git.context.payload)
+//     const commitMessage = git.context.payload.comment
+//     console.log('commit message', commitMessage)
+//     if (commitMessage.includes(skipOnCommitMsg)) {
+//       core.setFailed(`stopping here, because the commit message contains the provided input <${skipOnCommitMsg}>`)
+//     }
   } catch (error) {
     if(error instanceof Error){
       core.setFailed(error.message)
