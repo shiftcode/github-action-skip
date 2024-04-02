6e31bf0bea13e80f8bd1efdbe0611e9239bb8a93
vitaliibendyk <162116444+vitaliibendyk@users.noreply.github.com>
2024-04-02 16:16:52 +0200

    chore: update devDependencies, tslib and migrate to esm

--- prepare-husky.js
+++
@@ -1,8 +0,0 @@
-const { isGithubWorkflow } = require('@shiftcode/branch-utilities')
-
-if (isGithubWorkflow(process.env)) {
-  console.log(`won't install husky hooks since running in github action`)
-} else {
-  console.log('install husky hooks')
-  require('husky').install()
-}
